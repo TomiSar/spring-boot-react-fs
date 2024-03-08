1f328a8b4293d9358f7c8fb5a889ffad0b40dcae
Tomi Sarjamo <Tomi.Sarjamo@softability.fi>
2024-03-09 00:18:15 +0200

    Registration and Login functionality implemented in back-end.

---
+++ todo-project/todo-management/src/main/java/net/javaguides/todo/controller/AuthController.java
@@ -0,0 +1,33 @@
+package net.javaguides.todo.controller;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.todo.dto.LoginDto;
+import net.javaguides.todo.dto.RegisterDto;
+import net.javaguides.todo.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private AuthService authService;
+
+    // Register User REST API
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody RegisterDto registerDto){
+        String response = authService.register(registerDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    // Login User REST API
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(@RequestBody LoginDto loginDto) {
+        String response = authService.login(loginDto);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
